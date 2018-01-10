e3399902969b3f94763a6d9f72e67ad55653402a
samisnotinsane <risingdragon111@gmail.com>
2018-01-10 13:45:29 +0000

    Creating catkin workspace.

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lorejam/sameen/arq-teleop-robot/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/lorejam/sameen/arq-teleop-robot/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lorejam/sameen/arq-teleop-robot/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/lorejam/sameen/arq-teleop-robot/catkin_ws/src:$ROS_PACKAGE_PATH"
