121defd39704ef56071e005d30820553c8db6f30
samisnotinsane <risingdragon111@gmail.com>
2018-01-26 16:23:59 +0000

    Removing build and devel specific to lorejam-VirtualBox

--- catkin_ws/build/pan_tilt_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lorejam/sameen/arq-teleop-robot/catkin_ws/src/pan_tilt_cam
-# Build directory: /home/lorejam/sameen/arq-teleop-robot/catkin_ws/build/pan_tilt_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/pan_tilt_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lorejam/sameen/arq-teleop-robot/catkin_ws/src/pan_tilt_description
-# Build directory: /home/lorejam/sameen/arq-teleop-robot/catkin_ws/build/pan_tilt_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
