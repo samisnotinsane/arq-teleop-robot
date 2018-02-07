061456085cc5ba22ecca70f9093d721b12dd7b0e
WaelK <wael57@hotmail.co.uk>
2018-02-07 16:40:41 +0000

    Remove folders

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arq/wael/catkin_ws/src
-# Build directory: /home/arq/wael/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(kinect_description)
-subdirs(kinect_gazebo)
--- src/kinect_description/urdf/update_urdf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rosrun xacro xacro --inorder -o kineck.urdf kinect.urdf.xacro
-echo "URDF generated into file: kinect.urdf"
