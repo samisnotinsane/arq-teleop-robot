3a6f5d25ec83af9e60ae0d0a45d392a94d98803e
Lorenzo <sameen11@hotmail.com>
2018-02-07 16:24:18 +0000

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/arq/wael/catkin_ws/src
+# Build directory: /home/arq/wael/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(kinect_description)
+subdirs(kinect_gazebo)
