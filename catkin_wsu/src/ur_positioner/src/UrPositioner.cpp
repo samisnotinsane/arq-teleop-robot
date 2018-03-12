139365a2c85bf09b2da9fc224c98e1a17fa2226e
Nnadozie <n.e.okeke@outlook.com>
2018-03-12 21:18:38 +0000

    updated pacakge and Cmake for moveit --possibly incorrect but...

--- catkin_wsu/src/ur_positioner/src/UrPositioner.cpp
+++ catkin_wsu/src/ur_positioner/src/UrPositioner.cpp
@@ -1,6 +1,9 @@
 #include "ur_positioner/UrPositioner.hpp"
 #include <string>
 
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
 namespace ur_positioner {
 	UrPositioner::UrPositioner(ros::NodeHandle& nodeHandle)
 		: nodeHandle_(nodeHandle)
@@ -11,7 +14,10 @@ namespace ur_positioner {
 		}
 		subscriber_ = nodeHandle_.subscribe(subscriberTopic_, 1, &UrPositioner::topicCallback, this);
 		ROS_INFO("Successfully launched node.");
+		
+		moveit::planning_interface::MoveGroup group("ur_arm");		
 	}
+
 
 	UrPositioner::~UrPositioner() {
 	}
