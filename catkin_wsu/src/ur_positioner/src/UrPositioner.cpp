741262161c070ccc98eb7d6d0ce3e2e811df2ff3
Nnadozie <n.e.okeke@outlook.com>
2018-03-15 03:05:32 +0000

    robot moved

--- catkin_wsu/src/ur_positioner/src/UrPositioner.cpp
+++ catkin_wsu/src/ur_positioner/src/UrPositioner.cpp
@@ -12,6 +12,10 @@ namespace ur_positioner {
 	UrPositioner::UrPositioner(ros::NodeHandle& nodeHandle)
 		: nodeHandle_(nodeHandle)
 	{
+		
+		ros::AsyncSpinner spinner(1);
+  		spinner.start();
+
 		if(!readParameters()) {
 			ROS_ERROR("Could not read parameters.");
 			ros::requestShutdown();
@@ -46,12 +50,43 @@ namespace ur_positioner {
 
 		ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
 		ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
-		geometry_msgs::Pose target_pose1;
+		/**geometry_msgs::Pose target_pose1;
 		target_pose1.orientation.w = 1.0;
 		target_pose1.position.x = 0.28;
 		target_pose1.position.y = -0.7;
 		target_pose1.position.z = 1.0;
 		group.setPoseTarget(target_pose1);
+
+		moveit::planning_interface::MoveGroup::Plan my_plan;
+		bool success = group.plan(my_plan);
+
+		ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+		sleep(10.0);
+		ROS_INFO("Got here: 6");**/
+		moveit::planning_interface::MoveGroup::Plan my_plan;
+		std::vector<double> group_variable_values;
+		group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()), group_variable_values);
+
+		group_variable_values[0] = -1.0;
+		group.setJointValueTarget(group_variable_values);
+		ROS_INFO("got here: 6");
+		bool success = group.plan(my_plan);
+
+		ROS_INFO("Visualizing plan 2 (joint space goal) %s",success?"":"FAILED");
+		sleep(10.0);
+		group.move();
+
+		if (1) {
+    			ROS_INFO("Visualizing plan 1 (again)");    
+    			display_trajectory.trajectory_start = my_plan.start_state_;
+    			display_trajectory.trajectory.push_back(my_plan.trajectory_);
+    			display_publisher.publish(display_trajectory);
+    			/* Sleep to give Rviz time to visualize the plan. */
+    			sleep(5.0);
+  		}
+		
+
+
 	}
 
 
